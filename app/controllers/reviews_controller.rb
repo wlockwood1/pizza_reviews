33fe6cf846860abf796a7ee3978c22e2c5f6ac87
Will Lockwood <wrlockwood1@gmail.com>
2017-03-07 21:43:26 -0800

    Sort initial table by newest review, add smaller header instructing to click on marker

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,7 +2,7 @@ class ReviewsController < ApplicationController
   helper_method :sort_column, :sort_direction
 
   def index
-    @reviews = Review.all
+    @reviews = Review.all.order('review_date DESC')
 
     @hash = Gmaps4rails.build_markers(@reviews) do |review, marker|
       marker.lat review.latitude
